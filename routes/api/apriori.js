551d169bd4c5e2fac81a018dac659714dfd00883
Liam-Gardner <liam.gardner@hotmail.com>
2020-02-03 10:07:06 +0000

    work setup + more

--- routes/api/apriori.js
+++ routes/api/apriori.js
@@ -1,84 +1,100 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const spawn = require('child_process').spawn;
-require('dotenv').config();
+const path = require("path");
+const spawn = require("child_process").spawn;
+const fs = require("fs");
+require("dotenv").config();
 
 const setupODBC = () => {
   return new Promise((resolve, reject) => {
-    console.log('setting up odbc connection');
+    console.log("setting up odbc connection");
     let err = false;
     let bin =
-      'odbcconf.exe /Ld odbc_log.txt /a {CONFIGDSN "SQL Server Native Client 11.0" "DSN=association_rules_api|SERVER=ACER|Trusted_Connection=Yes|Database=Flipdish"}';
+      'odbcconf.exe /a {CONFIGDSN "SQL Server Native Client 11.0" "DSN=association_rules_api|SERVER=localhost|Trusted_Connection=Yes|Database=flipdishlocal"}';
     let cliArgs = [];
     let options = {
       spawn: true,
-      cwd: 'C:/',
+      cwd: "C:/",
       shell: true // this or shit breaks!
     };
 
     const child = spawn(bin, cliArgs, options);
-    child.stderr.on('data', data => {
+    child.stderr.on("data", data => {
       console.log(data.toString());
     });
-    child.stdout.on('data', data => {
+    child.stdout.on("data", data => {
       console.log(data.toString());
     });
-    child.on('error', error => {
+    child.on("error", error => {
       err = true;
       reject(error);
     });
-    child.on('exit', () => {
+    child.on("exit", () => {
       if (err) return; // debounce - already rejected
-      resolve('done.'); // TODO: check exit code and resolve/reject accordingly
+      resolve("done."); // TODO: check exit code and resolve/reject accordingly
     });
   });
 };
 
-const rscriptPath = path.resolve('./', 'R', 'apriori.R');
+const rscriptPath = path.resolve("./", "R", "apriori.R");
 const callR = (path, storeId) => {
   return new Promise((resolve, reject) => {
     let err = false;
     const child = spawn(process.env.RSCRIPT, [
-      '--vanilla',
+      "--vanilla",
       path,
-      '--args',
+      "--args",
       storeId
     ]);
-    child.stderr.on('data', data => {
+    child.stderr.on("data", data => {
       console.log(data.toString());
     });
-    child.stdout.on('data', data => {
+    child.stdout.on("data", data => {
       console.log(data.toString());
     });
-    child.on('error', error => {
+    child.on("error", error => {
       err = true;
       reject(error);
     });
-    child.on('exit', () => {
+    child.on("exit", () => {
       if (err) return; // debounce - already rejected
-      resolve('done.'); // TODO: check exit code and resolve/reject accordingly
+      resolve("done."); // TODO: check exit code and resolve/reject accordingly
     });
   });
 };
-router.post('/test', (req, res, next) => {
+
+const convertRulesToJson = () => {
+  rulesJson = fs.readFile('rules.json');
+  parsedJson = JSON.parse(rulesJson);
+  let lhs = parsedJson.lhs;
+  let rhs = parsedJson.rhs;
+  const keyValPairs = lhs.reduce((obj, value, index) => {
+    obj[value] = rhs[index];
+    return obj;
+  }, {});
+  return keyValPairs;
+};
+
+router.post("/test", (req, res, next) => {
   // create the db connection
   setupODBC()
     .then(result => {
-      console.log('odbc setup complete ', result);
-      console.log('Invoking R script at: ', rscriptPath);
+      console.log("odbc setup complete ", result);
+      console.log("Invoking R script at: ", rscriptPath);
+      console.log(req.body);
       callR(rscriptPath, req.body.storeId)
         .then(result => {
-          console.log('finished with callR: ', result);
-          res.status(200).send(result);
+          console.log("finished with callR: ", result);
+          const rules = convertRulesToJson();
+          res.status(200).send(rules);
         })
         .catch(error => {
-          console.log('Finished with callR - error: ', error);
+          console.log("Finished with callR - error: ", error);
           res.status(500).send(error);
         });
     })
     .catch(error => {
-      console.log('Finished with ODBC - error: ', error);
+      console.log("Finished with ODBC - error: ", error);
       res.status(500).send(error);
     });
 });
